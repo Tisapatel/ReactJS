1d046f2cbfd30a6401f69e26eb1059f4432bede1
tisa-patel <trpatel.sd21@gmail.com>
2025-11-18 23:58:29 +0530

    counter

---
+++ day-4-counter/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  // 1) Refresh par old value load karna
+  useEffect(() => {
+    const oldCount = localStorage.getItem("count");
+    if (oldCount !== null) {  
+      setCount(Number(oldCount));
+    }
+  }, []);
+
+  // 2) Jab count update ho tab localStorage me store karna
+  useEffect(() => {
+    if (count !== 0) {              // <-- YOUR CONDITION
+      localStorage.setItem("count", count);
+    }
+  }, [count]);
+
+  const handleIncrement = () => {
+  setCount(count + 1);
+};
+
+  return (
+    <>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrement}>Increase</button>
+    </>
+  );
+};
+
+export default App;
