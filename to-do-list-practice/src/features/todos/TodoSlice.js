027998d7facc7ba28740cf3044c43d792da40405
tisa-patel <trpatel.sd21@gmail.com>
2026-01-06 11:27:43 +0530

    todo-list

---
+++ to-do-list-practice/src/features/todos/TodoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+    name: 'todos',
+    initialState: [],
+    reducers: {
+        addTodo: (state, action) => {
+            state.push(action.payload);
+        },
+        removeTodos: (state, action) => {
+            return state.filter(todo => todo.id !== action.payload);
+        },
+    },
+});
+
+export const { addTodo, removeTodos } = todosSlice.actions;
+
+export default todosSlice.reducer;
