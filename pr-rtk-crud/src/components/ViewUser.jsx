e913cac2f43b8bd91a0d8a10df07ab6d30fc1374
tisa-patel <trpatel.sd21@gmail.com>
2026-01-10 22:56:13 +0530

    done

--- pr-rtk-crud/src/components/ViewUser.jsx
+++ pr-rtk-crud/src/components/ViewUser.jsx
@@ -1,53 +1,83 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteUser, editUser } from '../features/users/UserSlice';
+import '../css/style.css';
 
 const ViewUser = () => {
     const {users} = useSelector(state => state.users);
     const dispatch = useDispatch();
     
   return (
-    <div className='container'>
-    <div className='row justify-content-center'>
-        <div className='col-md-8'>
-            <div className='table-responsive'>
+    <div className='viewuser-container'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-11 col-lg-10'>
+            <div className='viewuser-card'>
+              
+              <div className='mb-4'>
+                <h2 className='viewuser-title'>View All Users</h2>
+                <p className='viewuser-subtitle'>Manage your users efficiently</p>
+              </div>
 
-                <table className='table table-bordered table-striped table-hover caption-top'>
-                    <caption><h2>View All Users.</h2></caption>
-                    <thead>
+              <div className='table-responsive'>
+                <table className='table table-dark-custom'>
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Username</th>
+                      <th>Email</th>
+                      <th>Password</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      users.length > 0 ? (
+                        users.map((user, index) => {
+                          const {id, username, email, password} = user;
+                          return (
+                            <tr key={id}>
+                              <td>{index + 1}</td>
+                              <td><strong>{username}</strong></td>
+                              <td>{email}</td>
+                              <td>{'•'.repeat(password?.length || 8)}</td>
+                              <td>
+                                <button 
+                                  type="button" 
+                                  onClick={() => dispatch(editUser(id))} 
+                                  className="btn btn-edit"
+                                >
+                                  Edit
+                                </button>
+                                <button 
+                                  type="button" 
+                                  onClick={() => dispatch(deleteUser(id))} 
+                                  className="btn btn-delete"
+                                >
+                                  Delete
+                                </button>
+                              </td>
+                            </tr>
+                          )
+                        })
+                      ) : (
                         <tr>
-                            <th>Sr. No</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                            <th>Action</th>
+                          <td colSpan="5">
+                            <div className='empty-state'>
+                              <div className='empty-state-text'>No users found</div>
+                              <p className='empty-state-subtext'>Add some users to get started</p>
+                            </div>
+                          </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user,index)=>{
-                                const {id,username,email,password} = user;
-                                return (
-                                    <tr key={id}>
-                                        <td>{index + 1}</td>
-                                        <td>{username}</td>
-                                        <td>{email}</td>
-                                        <td>{password}</td>
-                                        <td>
-                                            <button type="button" onClick={()=> dispatch(deleteUser(id))} className="btn btn-outline-danger">Delete</button>
-                                            {' '}
-                                            <button type="button" onClick={()=> dispatch(editUser(id))} className="btn btn-outline-warning">Edit</button>
-                                        </td>
-                                    </tr>
-                                )
-
-                            })
-                        }
-                    </tbody>
+                      )
+                    }
+                  </tbody>
                 </table>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
